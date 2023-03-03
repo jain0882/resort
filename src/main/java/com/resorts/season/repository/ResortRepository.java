6f8fbccbe4fcdad9f1cbdc184bc6f5575be52f59
Anurag Jain <103618830+jain0882@users.noreply.github.com>
2023-03-03 19:54:30 +0530

    Delete src directory

--- src/main/java/com/resorts/season/repository/ResortRepository.java
+++
@@ -1,9 +0,0 @@
-package com.resorts.season.repository;
-
-import com.resorts.season.model.Resort;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResortRepository extends JpaRepository<Resort, Long> {
-}
